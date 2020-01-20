b4cbc25c3f46d8ea45200e94e000b0303b2281e2
vKobylchak <vasyl.kobylchak@avenga.com>
2020-01-20 16:55:05 +0200

    update IndexController

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,14 +1,23 @@
 package guru.springframework.controllers;
 
+import guru.springframework.service.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @GetMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        System.out.println("Some message to say ... 1234 55555");
+    public String getIndexPage(Model model) {
+        model.addAttribute("recipies", recipeService.getRecipies());
         return "index";
     }
+
 }
