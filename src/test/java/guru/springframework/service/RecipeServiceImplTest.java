c5247e9f0d8975fe3d91f491756d5bf3c3f34a84
vKobylchak <vasyl.kobylchak@avenga.com>
2020-01-22 18:16:13 +0200

    Service Test

---
+++ src/test/java/guru/springframework/service/RecipeServiceImplTest.java
@@ -0,0 +1,42 @@
+package guru.springframework.service;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipies() {
+        Recipe recipe = new Recipe();
+        HashSet receipesData = new HashSet();
+        receipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(receipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipies();
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
